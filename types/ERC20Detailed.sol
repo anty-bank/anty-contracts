502100c5db3d645eab3988040bd3f892e481208f
0xantyqueen <98650619+0xantyqueen@users.noreply.github.com>
2022-03-12 12:20:55 -0500

    add dependencies

---
+++ types/ERC20Detailed.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+pragma solidity 0.7.5;
+
+import '../interfaces/IERC20.sol';
+
+abstract contract ERC20Detailed is IERC20 {
+    string private _name;
+    string private _symbol;
+    uint8 private _decimals;
+
+    constructor(
+        string memory name_,
+        string memory symbol_,
+        uint8 decimals_
+    ) {
+        _name = name_;
+        _symbol = symbol_;
+        _decimals = decimals_;
+    }
+
+    function name() public view override returns (string memory) {
+        return _name;
+    }
+
+    function symbol() public view override returns (string memory) {
+        return _symbol;
+    }
+
+    function decimals() public view override returns (uint8) {
+        return _decimals;
+    }
+}
